b3e7d711864eb75ac4c003b9aa2dfd469cddcda2
Matthew Leon <matthew.leon.tech@gmail.com>
2024-12-22 19:37:57 -0800

    Update Copyrights
    Signed-off-by: Matthew Leon <matthew.leon.tech@gmail.com>

--- src/vm_manager.py
+++ src/vm_manager.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
- 
-# Copyright 2024 InfraMatrix
 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#Copyright (c) 2024 InfraMatrix. All rights reserved.
+
+#The user ("Licensee") is hereby granted permission to use this software and
+#associated documentation files (the "Software"),
+#subject to the express condition that Licensee shall not, under any circumstances,
+#redistribute, sublicense, copy, transfer, publish, disseminate, transmit,
+#broadcast, sell, lease, rent, share, loan, or otherwise make available the
+#Software, in whole or in part, in any form or by any means, to any third party
+#without prior written consent from the copyright holder,
+#and any such unauthorized distribution shall constitute a material breach of this
+#license and result in immediate, automatic termination of all rights granted
+#hereunder.
 
 import sys
 import os
