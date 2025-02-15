9a0b0121b0c1b3f055f35c24079c8aba746ddb15
Matthew Leon <matthew.leon.tech@gmail.com>
2025-02-15 14:51:11 -0800

    Add dataplane node metrics exporter with grafana
    Signed-off-by: Matthew Leon <matthew.leon.tech@gmail.com>

--- run_observability_monitor.sh
+++ run_observability_monitor.sh
@@ -6,9 +6,13 @@
 
 main()
 {
-    sudo ./IGS_venv/bin/python3 observability/metrics_reporter.py &
+    sudo ./IGS_venv/bin/python3 observability/client/metrics_reporter.py &
 
-    sudo prometheus --config.file=observability/prometheus_server_conf.yml &
+    sudo prometheus --config.file=observability/server/prometheus_config.yml &
+
+    export GF_DASHBOARDS_DEFAULT_HOME_DASHBOARD_PATH=observability/server/grafana_node_dashboard.json                      ─╯
+    export GF_DATASOURCES_URL=http://localhost:9090
+    sudo grafana-server --homepath /usr/share/grafana --config=observability/server/grafana.ini &
 }
 
 main
